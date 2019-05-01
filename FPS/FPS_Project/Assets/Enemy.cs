b803b307874b60e8bee91f7014cff82c662823e5
aBraM-aBraM <49529106+aBraM-aBraM@users.noreply.github.com>
2019-05-01 15:09:49 +0300

    Added zombies
    Added zombie and animations

---
+++ FPS/FPS_Project/Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	GameObject target;
+
+	Animator anim;
+
+	[SerializeField]
+	private float stoppingDistance;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		anim = GetComponent<Animator>();
+		target = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update()
+	{
+		anim.SetFloat("velocity", agent.velocity.magnitude);
+		float dist = Vector3.Distance(transform.position, target.transform.position);
+		if (dist < stoppingDistance)
+		{
+			StopMoving();
+		}
+		else
+		{
+			GoToTarget();
+		}
+	}
+
+	void GoToTarget()
+	{
+		agent.isStopped = false;
+		agent.SetDestination(target.transform.position);
+		anim.SetBool("attackRange", false);
+	}
+
+	void StopMoving()
+	{
+		agent.isStopped = true;
+		anim.SetBool("attackRange", true);
+	}
+}
